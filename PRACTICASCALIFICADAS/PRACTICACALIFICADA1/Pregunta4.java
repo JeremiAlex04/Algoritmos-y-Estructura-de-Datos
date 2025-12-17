b5f8058afdb0589ab3f9556738e59407b77ae04a
Jeremialex04 <jeremialex04@gmail.com>
2025-12-17 11:10:39 -0500

    Version 3

---
+++ PRACTICASCALIFICADAS/PRACTICACALIFICADA1/Pregunta4.java
@@ -0,0 +1,26 @@
+package PRACTICASCALIFICADAS.PRACTICACALIFICADA1;
+/*
+Según el arreglo  A = [3, 8, 12, 5, 10]
+- Se requiere generar un recorrido
+- Dentro del recorrido se requiere  identificar a los impares
+- Debe imprimir solo los impares 
+*/
+public class Pregunta4 {
+    public static void main(String[] args) {
+        int  a[] = {3,8,12,5,10};
+        System.err.println("Coleccion de Nros: 3, 8, 12, 5, 10");
+        System.out.println("Recorrido de Nros Impares: ");
+        for (int i = 0; i< a.length; i++) {
+            System.out.println(".- "+a[i]);
+        }
+
+        System.out.println("\nEvaluar los impares e imprimir: ");
+        for (int i = 0; i< a.length; i++) {
+            if (a[i] % 2 == 0) {
+                
+            } else {
+                System.out.println(".- "+a[i]);
+            }
+        }
+    }
+}
