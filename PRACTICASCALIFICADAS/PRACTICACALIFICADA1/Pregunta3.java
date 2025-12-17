743b03d2e196d3e9a51d6b6fbae4d6323db142db
Jeremialex04 <jeremialex04@gmail.com>
2025-12-16 19:13:22 -0500

    version 4

---
+++ PRACTICASCALIFICADAS/PRACTICACALIFICADA1/Pregunta3.java
@@ -0,0 +1,30 @@
+package PRACTICASCALIFICADAS.PRACTICACALIFICADA1;
+/*
+PREGUNTA 3:
+Dado A = ["Carlos", "Miguel", "Bryan", "Andres", "Simon"]:
+- Declara el arreglo
+- Elimina el elemento en la posición 3 del arreglo 
+*/
+
+public class Pregunta3 {
+    public static void main(String[] args) {
+        String a[] = { "Carlos", "Miguel", "Andres", "Andres", "Simon" };
+        System.out.println("Antes: ");
+        for (int i = 0; i< a.length; i++) {
+            System.out.println("Posicion "+i+": "+a[i]);
+        }
+
+        System.out.println("\nDespues: ");
+
+        for (int i = 0; i< 10; i++){
+            if (i == 3){
+                a[i] = "";
+            }
+        }
+
+        for (int i = 0; i < a.length ; i++) {
+            System.out.println("Posicion "+i+": "+a[i]);
+        }
+
+    }
+}
